d1dee146e119902ef076bea6799d2d31651a113d
kirill <faceobito@yandex.ru>
2024-09-21 13:24:12 +0500

    add project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        string botToken = configuration["BotToken"];
+        
+        System.Console.ReadLine();
+    }
+}
