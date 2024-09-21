4f43b9da87972314c6c704a6712323b965405e6c
Igor Kotelnikov <igorkotelnikov360@gmail.com>
2024-09-21 17:29:33 +0500

    функция "Update" принимает собщение и отвечает на него сlient.StartReceiving(комент) Console.ReadLine(); шобы прога не закрывалась

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿using Telegram.Bot;
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
 
 namespace TrainBot
 {
@@ -6,12 +10,31 @@ namespace TrainBot
     {
         static void Main(string[] args)
         {
-                var config = Config.LoadConfig();
-                var botClient = new TelegramBotClient(config.TelegramBotToken);
-                Console.WriteLine($"Tokent Bot:{config.TelegramBotToken}");
-                Console.WriteLine("proverka");
+            var config = Config.LoadConfig();
+            var сlient = new TelegramBotClient(config.TelegramBotToken);
+            сlient.StartReceiving(Update, Error);//тема будет обрабатывать обновы и ошибки
+            Console.WriteLine($"Tokent Bot:{config.TelegramBotToken}");
+            Console.WriteLine("proverka");
+            Console.ReadLine();
 
+        }
 
+        async private static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token) 
+        {
+            var message = update.Message;
+            if (message != null):
+            {
+                if (message.Text.ToLower().Contains("прив"))
+                {
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Привет!");
+                    return;
+                }
+            }
+        }
+
+        async private static Task Error(ITelegramBotClient botClient, Update update, CancellationToken token)
+        {
+            
         }
     }
 }
